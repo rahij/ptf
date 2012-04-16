33541e1b6d2b83f1345e78e3595b462619f34f4c
rahij <rahij.ramsharan@gmail.com>
2012-04-17 04:17:17 +0800

    first commit

---
+++ src/net/learn2develop/Receiver/SmsReceive.java
@@ -0,0 +1,48 @@
+package net.learn2develop.Receiver;
+ 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
+import android.widget.Toast;
+import android.util.Log;
+ 
+
+
+@SuppressWarnings("deprecation")
+public class SmsReceive extends BroadcastReceiver
+{
+
+	
+	@Override
+    public void onReceive(Context context, Intent intent) 
+    {
+        
+		
+		//---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();        
+        SmsMessage[] msgs = null;
+        String str = "";            
+        if (bundle != null)
+        {
+            
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];            
+            for (int i=0; i<msgs.length; i++){
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
+                //str += "SMS from " + msgs[i].getOriginatingAddress();                     
+                //str += " :";
+                str += msgs[i].getMessageBody().toString();
+                str += "\n";        
+            }
+           
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+           try {   
+                GMailSender sender = new GMailSender("rahij.ramsharan@gmail.com", "rf16champ");
+                sender.sendMail("Test SMS", str,   "toca928neji@post.wordpress.com",   "");   
+            } catch (Exception e) {  Log.e("SendMail", e.getMessage(), e);     }    
+                
+                
+    }
+}}
